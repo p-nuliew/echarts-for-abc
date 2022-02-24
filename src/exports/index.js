62a2608ac9d75fe757430d6a80eebc9691ab5e71
chenweilun <chenwl@shinemo.com>
2022-02-24 19:53:15 +0800

    Initialized

---
+++ src/exports/index.js
@@ -0,0 +1,5 @@
+import Test from './Test/index.js'
+
+export {
+  Test
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import React, {Component} from 'react'
+import { Test } from './exports'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+export {
+  Test
 }
