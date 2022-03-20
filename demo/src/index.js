89299d0ce10b22027db37af97227e73d8ba10db6
chenweilun <chenwl@shinemo.com>
2022-03-20 10:29:01 +0800

    pref: 代码优化1

--- demo/src/index.js
+++ demo/src/index.js
@@ -3,12 +3,19 @@ import {render} from 'react-dom'
 
 import { Test, KLine } from '../../src'
 
+const propsConfig = {
+  yAxisSplitNumber: 6,
+  showTips: true,
+  canDrag: true,
+  canScroll: true
+}
+
 export default class Demo extends Component {
   render() {
     return <div>
       <h1>echarts-for-abc Demo</h1>
       <Test />
-      <KLine />
+      <KLine propsConfig={propsConfig} />
     </div>
   }
 }
